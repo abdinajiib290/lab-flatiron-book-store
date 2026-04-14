53afe85ed2fc06d9bfab7a11f47d0ae0e420f752
Najiib <najiibqoobey290@gmail.com>
2026-04-14 15:41:25 +0300

    the problem is still there

--- index.js
+++ index.js
@@ -62,8 +62,9 @@ for(let i=0; i<bookStore.books.length; i++){
 
     bookTitle.textContent = bookStore.books[i].title;
     bookAuthor.textContent = bookStore.books[i].author;
-    bookImage.setAttribute("src", bookStore.books[i].image);
-    // bookImage.src = bookStore.books[i].bookImage
+    bookImage.src = bookStore.books[i].bookImage
+    bookImage.src = bookStore.books[i].image;
+    bookImage.alt = bookStore.books[i].title;
 
     bookContainer.appendChild(bookTitle);
     bookContainer.appendChild(bookAuthor);
